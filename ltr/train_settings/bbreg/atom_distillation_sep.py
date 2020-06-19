5e80f1a70b3f496e4c1497c4ce5ef0ff9fc5cf70
Duolikun Danier <duolikun.danier.16@ucl.ac.uk>
2020-06-19 12:51:55 +0100

    update train setting files

--- ltr/train_settings/bbreg/atom_distillation_sep.py
+++ ltr/train_settings/bbreg/atom_distillation_sep.py
@@ -1,17 +1,17 @@
 import torch.nn as nn
 import torch.optim as optim
-from ltr.dataset import Lasot, TrackingNet, MSCOCOSeq, Got10k
+from ltr.dataset import Lasot, TrackingNet, MSCOCOSeq
 from ltr.data import processing, sampler, LTRLoader
 import ltr.models.bbreg.atom as atom_models
 from ltr import actors
-from ltr.trainers import LTRTrainer
+from ltr.trainers import LTRDistillationTrainer
 import ltr.data.transforms as tfm
-
+from ltr.models.loss import distillation
+from ltr.admin import loading
 
 def run(settings):
     # Most common settings are assigned in the settings struct
-    settings.description = 'ATOM IoUNet using the baseline ATOM* settings in [https://arxiv.org/abs/1909.12297].' \
-                           'Unlike standard ATOM, it employs the GMM-based proposal sampling and minor parameter changes.'
+    settings.description = 'ATOM distillation - backbone and regressor separately'
     settings.batch_size = 64
     settings.num_workers = 8
     settings.print_interval = 1
@@ -25,12 +25,11 @@ def run(settings):
 
     # Train datasets
     lasot_train = Lasot(settings.env.lasot_dir, split='train')
-    got10k_train = Got10k(settings.env.got10k_dir, split='vottrain')
-    trackingnet_train = TrackingNet(settings.env.trackingnet_dir, set_ids=list(range(4)))
+    trackingnet_train = TrackingNet(settings.env.trackingnet_dir, set_ids=list(range(11)))
     coco_train = MSCOCOSeq(settings.env.coco_dir)
 
     # Validation datasets
-    got10k_val = Got10k(settings.env.got10k_dir, split='votval')
+    trackingnet_val = TrackingNet(settings.env.trackingnet_dir, set_ids=list(range(11,12)))
 
     # The joint augmentation transform, that is applied to the pairs jointly
     transform_joint = tfm.Transform(tfm.ToGrayscale(probability=0.05))
@@ -44,7 +43,7 @@ def run(settings):
                                   tfm.Normalize(mean=settings.normalize_mean, std=settings.normalize_std))
 
     # Data processing to do on the training pairs
-    proposal_params = {'proposal_method': 'gmm', 'boxes_per_frame': 128, 'proposal_sigma': [(0.05, 0.05), (0.5, 0.5)]}
+    proposal_params = {'min_iou': 0.1, 'boxes_per_frame': 16, 'sigma_factor': [0.01, 0.05, 0.1, 0.2, 0.3]}
     data_processing_train = processing.ATOMProcessing(search_area_factor=settings.search_area_factor,
                                                       output_sz=settings.output_sz,
                                                       center_jitter_factor=settings.center_jitter_factor,
@@ -65,32 +64,68 @@ def run(settings):
                                                     joint_transform=transform_joint)
 
     # The sampler for training
-    dataset_train = sampler.ATOMSampler([lasot_train, got10k_train, trackingnet_train, coco_train], [1,1,1,1],
-                                samples_per_epoch=1000*settings.batch_size, max_gap=200, processing=data_processing_train)
+    dataset_train = sampler.ATOMSampler([lasot_train, trackingnet_train, coco_train], [1,1,1],
+                                samples_per_epoch=1000*settings.batch_size, max_gap=50, processing=data_processing_train)
 
     # The loader for training
     loader_train = LTRLoader('train', dataset_train, training=True, batch_size=settings.batch_size, num_workers=settings.num_workers,
                              shuffle=True, drop_last=True, stack_dim=1)
 
     # The sampler for validation
-    dataset_val = sampler.ATOMSampler([got10k_val], [1], samples_per_epoch=500*settings.batch_size, max_gap=200,
-                                      processing=data_processing_val)
+    dataset_val = sampler.ATOMSampler([trackingnet_val], [1], samples_per_epoch=500*settings.batch_size, max_gap=50,
+                              processing=data_processing_val)
 
     # The loader for validation
     loader_val = LTRLoader('val', dataset_val, training=False, batch_size=settings.batch_size, num_workers=settings.num_workers,
                            shuffle=False, drop_last=True, epoch_interval=5, stack_dim=1)
 
-    # Create network and actor
-    net = atom_models.atom_resnet18(backbone_pretrained=True)
-    objective = nn.MSELoss()
-    actor = actors.AtomActor(net=net, objective=objective)
+    # Load teacher network
+    teacher_net = atom_models.atom_resnet18(backbone_pretrained=True)
+    teacher_path = '/content/pytracking/pytracking/networks/atom_default.pth'
+    teacher_net = loading.load_weights(teacher_net, teacher_path, strict=True)
+    print('*******************Teacher net loaded successfully*******************')
+    
+    # Create student network
+    student_net = atom_models.atom_resnet18tiny(backbone_pretrained=False)
+
+    ##########################################################
+    ### Distil backbone first, turn off grad for regressor ###
+    ##########################################################
+    for p in student_net.bb_regressor.parameters():
+        p.requires_grad_(False)
+
+    objective = distillation.TSKDLoss(reg_loss=nn.MSELoss(), w_ts=0., w_ah=0., w_tr=1.)
+    actor = actors.AtomDistillationActor(student_net, teacher_net, objective)
 
     # Optimizer
-    optimizer = optim.Adam(actor.net.bb_regressor.parameters(), lr=1e-3)
+    optimizer = optim.Adam(actor.student_net.feature_extractor.parameters(), lr=1e-3)
+
     lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=15, gamma=0.2)
 
     # Create trainer
-    trainer = LTRTrainer(actor, [loader_train, loader_val], optimizer, settings, lr_scheduler)
+    trainer = LTRDistillationTrainer(actor, [loader_train, loader_val], optimizer, settings, lr_scheduler)
 
     # Run training (set fail_safe=False if you are debugging)
-    trainer.train(50, load_latest=True, fail_safe=True)
+    trainer.train(50, load_latest=False, fail_safe=False)
+
+    #########################################################
+    ### Distil regressor next, turn off grad for backbone ###
+    #########################################################
+    for p in student_net.bb_regressor.parameters():
+        p.requires_grad_(True)
+    for p in student_net.feature_extractor.parameters():
+        p.requires_grad_(False)
+
+    objective = distillation.TSKDLoss(reg_loss=nn.MSELoss(), w_ts=1., w_ah=0.1, w_tr=0.)
+    actor = actors.AtomDistillationActor(student_net, teacher_net, objective)
+
+    # Optimizer
+    optimizer = optim.Adam(actor.student_net.bb_regressor.parameters(), lr=1e-3)
+
+    lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=15, gamma=0.2)
+
+    # Create trainer
+    trainer = LTRDistillationTrainer(actor, [loader_train, loader_val], optimizer, settings, lr_scheduler)
+
+    # Run training (set fail_safe=False if you are debugging)
+    trainer.train(50, load_latest=True, fail_safe=False)
